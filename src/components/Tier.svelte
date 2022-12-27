6b1724035694c4f459284d6cb165485f962115fb
Omar Kadery <omarkadery@protonmail.com>
2022-12-27 12:31:58 -0500

    first commit

---
+++ src/components/Tier.svelte
@@ -0,0 +1,47 @@
+<script>
+  // @ts-nocheck
+
+  import { flip } from "svelte/animate";
+  import { dndzone } from "svelte-dnd-action";
+  export let items = [];
+  export let containerWidth = "200vw";
+  let randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+
+  const flipDurationMs = 300;
+  function handleDndConsider(e) {
+    items = e.detail.items;
+  }
+  function handleDndFinalize(e) {
+    items = e.detail.items;
+  }
+</script>
+
+<section
+  style="width:{containerWidth}"
+  use:dndzone={{ items, flipDurationMs }}
+  on:consider={handleDndConsider}
+  on:finalize={handleDndFinalize}
+>
+  {#each items as item (item.id)}
+    <div
+      class="item"
+      animate:flip={{ duration: flipDurationMs }}
+      style="background-color: #{randomColor()}"
+    >
+      {item.name}
+    </div>
+  {/each}
+</section>
+
+<style>
+  section {
+    background-color: #1a1a17;
+    min-height: 11em;
+    border: 2px solid black;
+  }
+  .item {
+    height: 11em;
+    width: 8em;
+    display: inline-block;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Tier from "../components/Tier.svelte";
+
+  let items = [
+    { id: 11, name: "item11" },
+    { id: 12, name: "item12" },
+    { id: 13, name: "item13" },
+    { id: 14, name: "item14" },
+    { id: 15, name: "item15" },
+    { id: 16, name: "item16" },
+    { id: 17, name: "item17" },
+  ];
+  let items2 = [
+    { id: 18, name: "item11" },
+    { id: 19, name: "item12" },
+    { id: 20, name: "item13" },
+    { id: 21, name: "item14" },
+    { id: 22, name: "item15" },
+    { id: 23, name: "item16" },
+    { id: 24, name: "item17" },
+  ];
+</script>
+
+<Tier {items} />
+<Tier items={items2} />
+<Tier items={[{ id: 25, name: "item25" }]} />
+<Tier items={[]} />
+<Tier items={[]} />
+<Tier items={[]} />

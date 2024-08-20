ee5a5f9747f0a0f3fd6163057b6e5eb8c8f58319
Omar Kadery <omarkadery@omar-linuxpad.attlocal.net>
2024-08-20 08:14:19 -0400

    add url for DigitalOcean droplet

--- src/routes/list/[id]/+page.svelte
+++ src/routes/list/[id]/+page.svelte
@@ -1,39 +1,38 @@
 <script>
-  // @ts-nocheck
+    // @ts-nocheck
 
-  import Tier from "../../../components/Tier.svelte";
-  import { onMount } from "svelte";
-  import * as api from "../../../constants/api.json";
-  import InitZone from "../../../components/InitZone.svelte";
+    import Tier from "../../../components/Tier.svelte";
+    import { onMount } from "svelte";
+    import InitZone from "../../../components/InitZone.svelte";
 
-  export let data;
+    export let data;
 
-  let items;
-  let steamid = data.id;
+    let items;
+    let steamid = data.id;
 
-  onMount(async () => {
-    fetch(
-      api.api_url +
-        new URLSearchParams({
-          steamid,
-        }),
-      {
-        method: "GET",
-      }
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        items = [];
-        for (let i = 0; i < data.response.games.length; i++) {
-          items.push({
-            id: Number(data.response.games[i].appid),
-            name: data.response.games[i].name,
-          });
-        }
-      });
-  });
+    onMount(async () => {
+        fetch(
+            "http://134.209.72.209:2375/games?" +
+                new URLSearchParams({
+                    steamid,
+                }),
+            {
+                method: "GET",
+            },
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                items = [];
+                for (let i = 0; i < data.response.games.length; i++) {
+                    items.push({
+                        id: Number(data.response.games[i].appid),
+                        name: data.response.games[i].name,
+                    });
+                }
+            });
+    });
 </script>
 
 <Tier items={[]} tierHeadingColor="#E8687B" tierHeadingLevel="S" />
